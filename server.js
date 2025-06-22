ea69d4d2776b4af4402b8cbcd1acfc7d68522563
schoolofteche <schooloftecheinfo@gmail.com>
2025-06-22 19:09:04 +0300

    exporting app and server

--- server.js
+++ server.js
@@ -97,9 +97,13 @@ app.use('/image', image);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is listening at http://0.0.0.0:${PORT}`);
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server is listening at http://0.0.0.0:${PORT}`);
+// });
+
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is listening at http://0.0.0.0:${PORT}`);
 });
 
-
+module.exports = { app, server };
 
--- test/serverTest.js
+++ test/serverTest.js
@@ -1,26 +1,26 @@
-process.env.NODE_ENV = 'test';   
+process.env.NODE_ENV = 'test';
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-
-var server = require('../server');
-var should = chai.should();
-var expect = chai.expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { app, server } = require('../server'); // 👈 import both
+const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Photos', function(){
+describe('Photos', function () {
+  it('should list ALL photos on / GET', function (done) {
+    this.timeout(60000);
+    chai.request(app) // 👈 use app, not server
+      .get('/')
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
+  });
 
-
-    it('should list ALL photos on / GET', function(done){
-        this.timeout(60000);
-        chai.request(server)
-        .get('/')
-        .end(function(err,res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('object')
-            done();
-        })
-    });
-})
+  after(() => {
+    if (server) server.close(); // 👈 clean exit if server exists
+  });
+});
